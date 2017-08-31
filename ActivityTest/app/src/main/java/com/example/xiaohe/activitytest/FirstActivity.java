017ff2fe340d48940da9b4992cb87c1abed5dda1
Hu <xiaohhu@784f438cf04a.ant.amazon.com>
2017-08-31 17:40:24 -0400

    Something new

--- ActivityTest/app/src/main/java/com/example/xiaohe/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/xiaohe/activitytest/FirstActivity.java
@@ -1,7 +1,11 @@
 package com.example.xiaohe.activitytest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,8 +23,34 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT). show();
+//                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT). show();
+//                finish();
+//                Intent intent = new Intent("com.example.activitytest.ACTION_START");
+//                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.amazon.com"));
+                startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
