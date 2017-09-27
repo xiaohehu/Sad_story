1b0c7aeb843551c2df34f4431986478adaa1313e
Xiaohe 小赫 <xiaohehu@users.noreply.github.com>
2017-09-27 11:06:35 -0400

    A little progress

--- ActivityTest/app/src/main/java/com/example/xiaohe/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/xiaohe/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,14 +25,40 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick (View v) {
 //                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT). show();
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                String data = "Hello SecondActivity";
+//                intent.putExtra("extra_data", data);
+
 //                finish();
+
 //                Intent intent = new Intent("com.example.activitytest.ACTION_START");
 //                intent.addCategory("com.example.activitytest.MY_CATEGORY");
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.amazon.com"));
-                startActivity(intent);
+
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.amazon.com"));
+
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10000"));
+
+//                startActivity(intent);
+
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 
     @Override
