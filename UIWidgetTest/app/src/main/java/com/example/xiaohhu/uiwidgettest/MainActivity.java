6f63a81e4c97bb84371294bdbf69177b0cc762bc
Xiaohe 小赫 <xiaohehu@users.noreply.github.com>
2017-10-10 14:45:07 -0400

    Added EditText view

--- UIWidgetTest/app/src/main/java/com/example/xiaohhu/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/xiaohhu/uiwidgettest/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.xiaohhu.uiwidgettest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 // 如果用switch语句要改这个activity的extends
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText) findViewById(R.id.exit_test);
         Button button = (Button) findViewById(R.id.button);
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -28,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.button:
                 // 此处添加逻辑
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
