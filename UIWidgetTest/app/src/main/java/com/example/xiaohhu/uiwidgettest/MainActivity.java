aa87aafc69ee3c9657b2c8d328e752b08b3b32f6
Xiaohe 小赫 <xiaohehu@users.noreply.github.com>
2017-10-10 11:59:54 -0400

    Added button

--- UIWidgetTest/app/src/main/java/com/example/xiaohhu/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/xiaohhu/uiwidgettest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.xiaohhu.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // 此处添加逻辑
+//            }
+//        });
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick (View v) {
+        switch (v.getId()){
+            case R.id.button:
+                // 此处添加逻辑
+                break;
+            default:
+                break;
+        }
     }
 }
