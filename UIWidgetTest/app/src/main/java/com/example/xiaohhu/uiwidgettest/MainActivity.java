1bfe91ff5873454f1e0d3c8cee95822309023f02
Xiaohe 小赫 <xiaohehu@users.noreply.github.com>
2017-11-08 18:24:27 -0500

    Toggle with image

--- UIWidgetTest/app/src/main/java/com/example/xiaohhu/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/xiaohhu/uiwidgettest/MainActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 // 如果用stestwitch语句要改这个activity的extends
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
-
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         editText = (EditText) findViewById(R.id.exit_test);
         Button button = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        imageView.setTag(1);
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -34,8 +37,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.button:
                 // 此处添加逻辑
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+//                String inputText = editText.getText().toString();
+//                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+//                imageView.setImageResource(R.drawable.image_2);
+                if ((Integer)imageView.getTag() == 1) {
+                    imageView.setTag(2);
+                    imageView.setImageResource(R.drawable.image_2);
+                } else {
+                    imageView.setTag(1);
+                    imageView.setImageResource(R.drawable.image_1);
+                }
                 break;
             default:
                 break;
