5f7ce8d406406720dd07cec3941f7dd5d67622ad
Xiaohe 小赫 <xiaohehu@users.noreply.github.com>
2017-11-29 16:30:37 -0500

    Added self-defined top navi-bar

--- UILayoutTest/app/src/main/java/com/example/xiaohhu/uilayouttest/MainActivity.java
+++ UILayoutTest/app/src/main/java/com/example/xiaohhu/uilayouttest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.xiaohhu.uilayouttest;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //隐藏掉自带的标题栏
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
