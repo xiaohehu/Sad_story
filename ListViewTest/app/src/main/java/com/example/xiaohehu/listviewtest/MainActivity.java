4e4546acb424b2f195011934fb5474b484370acb
Xiaohe 小赫 <xiaohehu@users.noreply.github.com>
2018-03-06 15:14:58 -0500

    IDE update

--- ListViewTest/app/src/main/java/com/example/xiaohehu/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/xiaohehu/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.xiaohehu.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
 
-        listView.setOnItemmClickListener (new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener (new AdapterView.OnItemClickListener() {
             @Override
                 public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
                 Fruit fruit = fruitList.get(position);
