75ead97239aaa5848a3574feac7f4045b2554703
Xiaohe Hu <huxiaohe113@gmail.com>
2018-03-10 21:03:35 -0500

    Added interaction to RecyclerView

--- RecyclerViewTest/app/src/main/java/com/example/xiaohehu/recyclerviewtest/FruitAdapter.java
+++ RecyclerViewTest/app/src/main/java/com/example/xiaohehu/recyclerviewtest/FruitAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,11 +19,13 @@ public class FruitAdapter extends RecyclerView. Adapter< FruitAdapter.ViewHolder
     private List<Fruit> mFruitList;
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+        View fruitView;
         ImageView fruitImage;
         TextView fruitName;
 
         public ViewHolder (View view) {
             super(view);
+            fruitView = view;
             fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
             fruitName = (TextView) view.findViewById(R.id.fruit_name);
         }
@@ -35,7 +38,15 @@ public class FruitAdapter extends RecyclerView. Adapter< FruitAdapter.ViewHolder
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.fruitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(view.getContext(), "You clicked on " + fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return holder;
     }
 
