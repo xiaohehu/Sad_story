dbfc53de51f80a0677989f35a04139b1450f6a92
Xiaohe Hu <huxiaohe113@gmail.com>
2018-03-14 20:33:53 -0400

    Added some pics to UIProject

--- UIBestPractice/app/src/main/java/com/example/xiaohehu/uibestpractice/MainActivity.java
+++ UIBestPractice/app/src/main/java/com/example/xiaohehu/uibestpractice/MainActivity.java
@@ -2,8 +2,21 @@ package com.example.xiaohehu.uibestpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Msg> msgList = new ArrayList<>();
+    private EditText inputText;
+    private Button send;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
