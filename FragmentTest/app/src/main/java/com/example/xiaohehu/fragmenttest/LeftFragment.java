373a614199bf8b750d6658b6fef28e1a41adb25f
Xiaohe Hu <huxiaohe113@gmail.com>
2018-03-19 00:07:29 -0400

    Fragment not showing

--- FragmentTest/app/src/main/java/com/example/xiaohehu/fragmenttest/LeftFragment.java
+++ FragmentTest/app/src/main/java/com/example/xiaohehu/fragmenttest/LeftFragment.java
@@ -1,16 +1,16 @@
 package com.example.xiaohehu.fragmenttest;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
 
 /**
  * Created by xiaohehu on 3/18/18.
  */
 
-public class LeftFragment extends android.support.v4.app.Fragment {
+public class LeftFragment extends Fragment {
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.left_fragment, container, false);
