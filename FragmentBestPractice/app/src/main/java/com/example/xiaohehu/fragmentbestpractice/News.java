060e0630f83c847f63a67e06f92680e86d8625d9
Xiaohe Hu <huxiaohe113@gmail.com>
2018-03-20 00:26:44 -0400

    Added News class and layout

---
+++ FragmentBestPractice/app/src/main/java/com/example/xiaohehu/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.xiaohehu.fragmentbestpractice;
+
+/**
+ * Created by xiaohehu on 3/19/18.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
